944758cd2dc0f19b0cbcfacd458d8ee2620fd6bb
Evert van Nieuwenburg <evert@caltech.edu>
2021-01-13 06:21:53 -0800

    Initial commit

---
+++ hamiltonianflow/flow.py
@@ -0,0 +1,50 @@
+from MBLHamiltonian import *
+
+L = int(sys.argv[1])
+hscale = float(sys.argv[2])
+Jscale = float(sys.argv[3])
+Uscale = float(sys.argv[4])
+seed = int(sys.argv[5])
+np.random.seed(seed)
+
+H = MBLHamiltonian(L,hscale, Jscale, Uscale)
+
+print("Starting H")
+for t in H.H.opterms:
+    print(t)
+
+h = []
+J = []
+energyscales = []
+hstep, Jstep = H.getCoefficientDistributions()
+h.append(hstep)
+J.append(Jstep)
+
+maxSteps = 300
+currentStep = 0
+finished = False
+
+while not finished:
+    finished, energyscale = H.rotateOut()
+    energyscales.append(energyscale)
+
+    hstep, Jstep = H.getCoefficientDistributions()
+    h.append(hstep)
+    J.append(Jstep)
+
+    currentStep += 1
+    if currentStep > maxSteps:
+        break
+
+    if (currentStep % 10 == 0) and (currentStep != 0):
+        np.save("data/W-{2}/chkpts/flowresult-L-{0}-seed-{1}.npy.chkpt".format(L,seed,hscale), np.array([h,J]), allow_pickle=True)
+        np.savetxt("data/W-{2}/chkpts/energyscales-L-{0}-seed-{1}.txt.chkpt".format(L,seed,hscale), np.array(energyscales))
+
+    print(currentStep)
+    for k in Jstep.keys():
+        print("range ", k)
+        print(np.mean(Jstep[k]))
+    print("")
+
+np.save("data/W-{2}/flowresult-L-{0}-seed-{1}.npy".format(L,seed,hscale), np.array([h,J]), allow_pickle=True)
+np.savetxt("data/W-{2}/energyscales-L-{0}-seed-{1}.txt".format(L,seed,hscale), np.array(energyscales))
---
+++ hamiltonianflow/launch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#for i in `seq 300 599`
+#do
+#	qsub -v L=6,hscale=1,Jscale=3,Uscale=1,seed=$i -N flow-W1-$i controlscript.pbs
+#	qsub -v L=6,hscale=4,Jscale=1,Uscale=1,seed=$i -N flow-W4-$i controlscript.pbs
+#done
+
+for i in `seq 0 599`
+do
+	qsub -v L=6,hscale=3,Jscale=1,Uscale=1,seed=$i -N flow-W3-$i controlscript.pbs
+done
+
+
+#for i in `seq 0 9`
+#do
+#	qsub -v L=8,hscale=1,Jscale=3,Uscale=1,seed=$i -N flow-W1-$i controlscript.pbs
+#	qsub -v L=8,hscale=4,Jscale=1,Uscale=1,seed=$i -N flow-W4-$i controlscript.pbs
+#done
